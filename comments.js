45d879d2615a98eef0ba1b63807db682086ca220
ronzinms <112196514+ronzinms@users.noreply.github.com>
2024-06-04 20:00:41 +0000

    Copilot comments.js commit

--- comments.js
+++ comments.js
@@ -1,19 +1,4 @@
 // create web server
-// use express module
-const express = require('express');
-const app = express();
-
-// use body-parser module
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// use cors module
-const cors = require('cors');
-app.use(cors());
-
-// use dotenv module
-require('dotenv').config();
-
-// use mysql module
-const mysql = require('mysql');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
